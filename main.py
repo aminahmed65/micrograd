664178a958bbdd6103c0c7af90c0cbe8256ab6e6
amin ahmed <48972428+aminahmed65@users.noreply.github.com>
2023-03-01 05:05:52 +0000

    created main.py

---
+++ main.py
@@ -0,0 +1,177 @@
+import math
+import numpy as np
+import random
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+    
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), _op = '+')
+        
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out =  Value(self.data * other.data, (self, other), _op = '*')
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self,other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data ** other, (self, ), label=f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other): # fixes other + self  (2.__add__(a))
+        return self + other
+    
+    def __rmul__(self, other): # fixes other * self  (2.__mul__(a))
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __neg__(self): # -self
+        return self * -1
+    
+    def __sub__(self,other): # self - other
+        return self + (-other)
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) -1 )/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+        out._backward = _backward
+        return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+
+
+    def backward(self):
+        
+        # Topological sorting for a grap (Making lay out Left to right) to prep for backprop
+
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node._backward() 
+
+
+class Neuron:
+
+    def __init__(self, nin): # nin = number of inputs into the neuron
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b =  Value(random.uniform(-1,1))
+
+    def __call__(self, x): # Neuron(x)
+        # w * x + b
+        act = sum((wi * xi for wi, xi in zip(self.w,x)), self.b)
+        out = act.tanh()
+        return out
+    
+    def parameters(self):
+        return self.w + [self.b]
+    
+# MLP
+
+class Layer:
+    def __init__(self, nin, nout): # nout = number of outputs (aka how many neurons do u want in this specific layer)
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self, x): # Layer(x)
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+    
+class MLP: # Multi Layer Perceptron
+    def __init__(self, nin , nouts): # nouts is a list in this case
+        sz = [nin] + nouts # size?
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+    
+
+
+# MLP Implementation
+
+x =[2.0, 3.0]
+n = MLP(3, [4 , 4 , 1])
+n(x)
+
+xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0],
+]
+
+ys = [1.0, -1.0, -1.0, 1.0] # desired targets (binary classifier neural network)
+
+for k in range(20):
+
+    # forward pass
+    ypred = [n(x) for x in xs]
+    loss = sum((yout - ygt)**2 for ygt, yout in zip(ys,ypred))  # mean squared error loss.  - ygt = y ground truth - low loss is better
+    loss
+
+    # backward pass
+    for p in n.parameters():
+        p.grad = 0.0
+    loss.backward()
+    
+    # update
+    for p in n.parameters():
+        p.data += -0.05 * p.grad
+
+    print(k, loss.data)
